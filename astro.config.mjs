cede05724d6f5c92a006477fba3b591f05a8d295
jonm <jonathan.edmund@gmail.com>
2025-11-29 22:03:51 +0700

    :rocket: deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://jonmun.com",
   integrations: [mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
